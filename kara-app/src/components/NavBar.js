46a79a389159ee75b625e6c0d012d03ba072f81a
2ndborn <micah.francis69@googlemail.com>
2025-05-30 06:26:19 +0100

    create staggered fade up for individual nalinks

--- kara-app/src/components/NavBar.js
+++ kara-app/src/components/NavBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {motion} from 'framer-motion';
 
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
@@ -8,9 +9,11 @@ import styles from '../styles/NavBar.module.css'
 
 import useClickOutsideToggle from '../hooks/useClickOutsideToggle';
 import { NavLink } from 'react-router-dom';
+import useFadeUp from '../hooks/useFadeUp';
 
 function NavBar() {
   const {expanded, setExpanded, ref} = useClickOutsideToggle();
+  const isLoaded = useFadeUp(2000)
   return (
     <div className={styles.NavContainer}>
         <Title />
@@ -19,13 +22,44 @@ function NavBar() {
             <Navbar.Toggle
               className='ms-auto' aria-controls="basic-navbar-nav" />
             <Navbar.Collapse className='ms-auto' id="basic-navbar-nav">
+            {isLoaded && (
               <Nav className={expanded ? "ms-auto d-flex flex-column align-items-end" : "mx-auto"}>
-                <NavLink to="/" className={styles.NavLink}>Home</NavLink>
-                <NavLink to="/mystory" className={styles.NavLink}>My story</NavLink>
-                <NavLink to="" className={styles.NavLink}>Services</NavLink>
-                <NavLink to="" className={styles.NavLink}>Gallery</NavLink>
-                <NavLink to="" className={styles.NavLink}>Contact me</NavLink>
+                <motion.div
+                  initial={{ opacity: 0, y: 20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 1, delay: 0.2, ease: "easeOut" }}>
+                  <NavLink to="/" className={styles.NavLink}>Home</NavLink>
+                </motion.div>
+
+                <motion.div
+                  initial={{ opacity: 0, y: 20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 1, delay: 0.4, ease: "easeOut" }}>
+                  <NavLink to="/mystory" className={styles.NavLink}>My story</NavLink>
+                </motion.div>
+
+                <motion.div
+                  initial={{ opacity: 0, y: 20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 1, delay: 0.6, ease: "easeOut" }}>
+                  <NavLink to="" className={styles.NavLink}>Services</NavLink>
+                </motion.div>
+
+                <motion.div
+                  initial={{ opacity: 0, y: 20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 1, delay: 0.8, ease: "easeOut" }}>
+                  <NavLink to="" className={styles.NavLink}>Gallery</NavLink>
+                </motion.div>
+
+                <motion.div
+                  initial={{ opacity: 0, y: 20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 1, delay: 1, ease: "easeOut" }}>
+                  <NavLink to="" className={styles.NavLink}>Contact me</NavLink>
+                </motion.div>
               </Nav>
+            )}
             </Navbar.Collapse>
           </Container>
         </Navbar>
