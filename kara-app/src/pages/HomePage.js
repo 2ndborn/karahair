43475ce31ddef36d493e612d03562e736be27436
2ndborn <micah.francis69@googlemail.com>
2025-05-31 08:50:59 +0100

    animated the hero image to scale on loading

--- kara-app/src/pages/HomePage.js
+++ kara-app/src/pages/HomePage.js
@@ -1,12 +1,25 @@
 import React from 'react'
+import { motion } from 'motion/react';
 import styles from '../styles/HomePage.module.css';
 import practiceImage from '../assets/practice-image.jpg'
 
 function HomePage() {
+  const variants = {
+    hidden: { opacity: 0, scale: 0.5 },
+    visible: { opacity: 1, scale: 1 },
+  }
+
   return (
-    <div className={styles.homeContainer}>
-      <img src={practiceImage} alt='out' />
-    </div>
+    <motion.div className={styles.homeContainer}>
+      <motion.img
+        src={practiceImage}
+        alt='out'
+        initial="hidden" // Reference the variants
+        animate="visible"
+        variants={variants}
+        transition={{ duration: 1, delay: 1.5, ease: "easeOut" }}
+      />
+    </motion.div>
   )
 }
 
