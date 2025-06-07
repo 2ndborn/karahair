d18ca0f0cf19306d69a900c2639f968a09985b34
2ndborn <micah.francis69@googlemail.com>
2025-06-07 15:02:50 +0100

    Making amendments to the home text

--- kara-app/src/pages/HomePage.js
+++ kara-app/src/pages/HomePage.js
@@ -1,72 +1,53 @@
 import React from 'react'
 import { motion } from 'motion/react';
 import styles from '../styles/HomePage.module.css';
-import practiceImage from '../assets/practice_image1.webp';
+import peach from '../assets/peach.jpg';
 
 function HomePage() {
   const homeContent = [
-    {title: "Knowledgeble", subtitle: "I bring over 2 decades of hair care experience with me. YOUR SAFE IN MY HANDS."},
-    {title: "Assessment", subtitle: "Using the perfected Numan method of consultation."},
-    {title: "Restores", subtitle: "Let take you on a journey to have you see what you really feel."},
-    {title: "Allure", subtitle: "You radiate elegance and prestige infecting others with your glow."}
+    {title: "KNOWLEDGE", subtitle: "I bring over 2 decades of hair care experience with me. YOUR SAFE IN MY HANDS."},
+    {title: "ASSESSMENT", subtitle: "Using the perfected Numan method of consultation."},
+    {title: "RESTORE", subtitle: "Let take you on a journey to have you see what you really feel."},
+    {title: "ALLURE", subtitle: "You radiate elegance and prestige infecting others with your glow."}
   ];
   const variants = {
     hidden: { opacity: 0, scale: 0.5 },
     visible: { opacity: 1, scale: 1 },
   }
 
-  const sloganVarients = {
-    hidden: {opacity: 0, y: 75},
-    visible: {opacity: 0.8},
-  }
-
   return (
     <motion.div className={styles.homeContainer}>
       <motion.img
-        src={practiceImage}
+        src={peach}
         alt='out'
         initial="hidden" // Reference the variants
         animate="visible"
         variants={variants}
         transition={{ duration: 1, delay: 1.5, ease: "easeOut" }}
       />
-      <motion.div
-        initial="hidden"
-        variants={sloganVarients}
-        whileInView={{opacity: 0.8, y: 0}}
-        transition={{ duration: 1.5, ease: "easeOut" }}
-        className={styles.Slogan}
-      >
+      <motion.div className={styles.Slogan}>
         <motion.ul className='list-unstyled mb-0 mx-5'>
           {homeContent.map((content, index) => (
             <motion.li
               key={index}
               initial="hidden"
               whileInView="visible"
-              viewport={{ once: true, amount: 0.3 }} // Animates when 30% of the element is visible
+              viewport={{ once: true, amount: 0.5 }} // Animates when 30% of the element is visible
               variants={{
                 hidden: { opacity: 0 },
-                visible: { opacity: 1, y: 0, transition: { staggerChildren: 0.2 }},
+                visible: { opacity: 1},
               }}
-              className='my-5'
             >
             <motion.h1
               initial={{opacity: 0, x: 250}}
-              whileInView={{opacity: 1, x: 0}}
-              transition={{delay: index * 0.8, duration: 0.8}}
+              animate={{opacity: 1, x: 0}}
+              transition={{delay: index * 0.8, duration: 0.8, type: "spring", bounce: 0.75}}
               style={{fontSize: "clamp(2rem, 5vw, 4.5rem)"}}
-              className='text-md-start text-cente5'
+              className='text-md-start text-center'
             >
               {content.title}
             </motion.h1>
-            <motion.h5
-              initial={{opacity: 0, x: -250}}
-              whileInView={{opacity: 1, x: 0}}
-              transition={{delay: index * 0.8 + 0.3, duration: 0.8 }}
-              className='text-md-end text-center'
-            >
-              {content.subtitle}
-            </motion.h5>
+            
           </motion.li>
           ))}
         </motion.ul>
