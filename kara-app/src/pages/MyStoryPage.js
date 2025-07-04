df973125db5a2e446a1bb8457c210e60678ee855
2ndborn <micah.francis69@googlemail.com>
2025-07-04 13:58:44 +0100

    Added another section to the story page

--- kara-app/src/pages/MyStoryPage.js
+++ kara-app/src/pages/MyStoryPage.js
@@ -1,17 +1,33 @@
 import React from 'react'
 import kara from '../assets/karapic.webp'
 import styles from '../styles/MyStory.module.css'
+import Container from 'react-bootstrap/Container'
+import {Row, Col} from 'react-bootstrap/'
 
 const MyStoryPage = () => {
   return (
+    <>
+    <section>
     <div className={styles.tcontainer}>
-        <img
-          src={kara}
-          alt='owner'
-          className={styles.heroImage}
-        />
-        <div className={styles.gradientBg}></div>
+      <img
+        src={kara}
+        alt='owner'
+        className={styles.heroImage}
+      />
+      <div className={styles.gradientBg}>
+        <div className={styles.leftHalf}>
+          <h1>"I am Kara"</h1>
+          <h3>xxxxx</h3>
+        </div>
+      </div>
     </div>
+    </section>
+    <section>
+      <div>
+        <div className={styles.box}></div>
+      </div>
+    </section>
+    </>
   )
 }
 
