3d8f5c1b024a104fe2d57d9588061e5edba7de9d
2ndborn <micah.francis69@googlemail.com>
2025-07-07 21:14:25 +0100

    Added responsiveness the home page

--- kara-app/src/pages/MyStoryPage.js
+++ kara-app/src/pages/MyStoryPage.js
@@ -6,6 +6,7 @@ import Container from 'react-bootstrap/Container'
 import {Row, Col} from 'react-bootstrap/'
 import useDynamicColor from '../hooks/useDynamicColor'
 import useFadeUp from '../hooks/useFadeUp'
+import Title from '../components/Title'
 
 const MyStoryPage = () => {
   const getColor = useDynamicColor();
@@ -13,37 +14,38 @@ const MyStoryPage = () => {
 
   return (
     <>
-      <section className={styles.sectionContainer}>
-        {isLoaded &&  (
-          <>
-          <motion.div 
+      <Title title="MY" subtitle="STORY" />
+          {/* <motion.div 
           className={styles.TitleContainer}
           style={{backgroundColor: getColor("title")}}
           >
           <h1>MY</h1>
           <h5>STORY</h5>
 
-        </motion.div>
-        <div className={styles.tcontainer}>
+        </motion.div> */}
+        <div id='next-section' className={styles.tcontainer}>
           <img
             src={kara}
             alt='owner'
             className={styles.heroImage}
             />
           <div className={styles.gradientBg}>
-            <div className={styles.leftHalf}>
-              <motion.h1
+            <div className={`${styles.leftHalf} px-5`}>
+              <motion.h3
               initial={{ opactiy: 0 }}
               animate={{ opacity: 1 }}
               transition={{ duration: 2.5, delay: 4, ease: "easeIn"}}
-               className='display-3'>"I am Kara"</motion.h1>
-              <h3>xxxxx</h3>
+              >
+                its all about that moment when a client looks into
+                the mirror and staring back at them is an actualised
+                version of their internal self.
+              </motion.h3>
+              <div style={{ width: "100%", textAlign: "right" }}>
+                <h5 className='text-right'>- Kara Francis</h5>
+              </div>
             </div>
           </div>
         </div>
-        </>
-        )}
-      </section>
     {/* <section>
       <div className={styles.con}>
         <div className={styles.con2}>
