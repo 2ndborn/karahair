7ed36fe7d136f33230600a8818d525b23fb663ce
2ndborn <micah.francis69@googlemail.com>
2025-07-29 21:28:22 +0100

    Restructured the grid for the service page

--- kara-app/src/pages/Services.js
+++ kara-app/src/pages/Services.js
@@ -1,12 +1,28 @@
 import React from 'react'
 import styles from '../styles/Services.module.css';
+import education from '../assets/karaeducation.webp';
+import roc from '../assets/roc.webp';
 
 const Services = () => {
   return (
     <div className={styles.GridContainer}>
         <div className={styles.header}>Services</div>
-        <div className={styles.hair}>Hair</div>
-        <div className={styles.educate}>Education</div>
+        <div className={styles.hair}>
+          <img className={styles.hairImage} src={roc} alt='Curly model'/>
+          <div className={styles.hairCover}>
+            <h1>
+              Haircare
+            </h1>
+          </div>
+        </div>
+        <div className={styles.educate}>
+          <img className={styles.edImage} src={education} alt='Students' />
+          <div className={styles.edCover}>
+            <h1>
+              Education
+            </h1>
+          </div>
+        </div>
         <div className={styles.codes}>Affiliate Codes</div>
     </div>
   )
