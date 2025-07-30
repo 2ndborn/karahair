58533698c99cba499c898363fa0a83ef0d8d8f89
2ndborn <micah.francis69@googlemail.com>
2025-07-30 07:03:38 +0100

    Added box and text shadows to the services page

--- kara-app/src/hooks/useDynamicColor.js
+++ kara-app/src/hooks/useDynamicColor.js
@@ -4,7 +4,8 @@ const useDynamicColor = () => {
     const location = useLocation();
     const getColor = (element) => {
         const colors = {
-            "/mystory": {navbar: "rgba(217, 185, 160, 0.1)", title: "#d9b9a0", footer: "#d9b9a0"},
+            "/mystory": {navbar: "rgba(217, 185, 160, 0.5)", title: "#d9b9a0", footer: "#d9b9a0"},
+            "/services": {navbar: "rgba(9, 8, 8, 0.9)", title: "#000", footer: "#fff"},
             default: {navbar: "transparent", title: "#f1e7dd", footer: "#f1e7dd"},
         }
         return colors[location.pathname]?.[element] || colors.default[element]
