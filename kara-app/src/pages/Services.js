0d3409c7e9f5543f09bf6346f228b47f525db0c4
2ndborn <micah.francis69@googlemail.com>
2025-07-31 08:15:11 +0100

    Animated the hair grid item

--- kara-app/src/pages/Services.js
+++ kara-app/src/pages/Services.js
@@ -1,23 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from '../styles/Services.module.css';
+import {LayoutGroup, motion} from 'framer-motion';
+
 import education from '../assets/karaeducation.webp';
 import roc from '../assets/roc.webp';
+
 import Title from '../components/Title';
 
 const Services = () => {
+
+  const [isToggled, setIsToggled] = useState(null);
+
+  const handleClick = (section) => {
+    setIsToggled(section);
+  }
+
+  const handleClose = () => {
+      setIsToggled(null);
+  }
+
   return (
     <>
       <Title title="Services" />
       <div id='next-section' className={styles.GridContainer}>
         <div className={styles.header}>Services</div>
-        <div className={styles.hair}>
-          <img className={styles.hairImage} src={roc} alt='Curly model' />
-          <div className={styles.hairCover}>
-            <h1>
-              Haircare
-            </h1>
-          </div>
-        </div>
+        <LayoutGroup>
+          <motion.div
+            layoutId='hair'
+            onClick={() => handleClick('hair')}
+            className={styles.hair}
+           >
+            <img className={styles.hairImage} src={roc} alt='Curly model' />
+            <div
+              onClick={handleClick}
+              className={styles.hairCover}>
+              <h1>
+                Haircare
+              </h1>
+            </div>
+          </motion.div>
+          </LayoutGroup>
         <div className={styles.educate}>
           <img className={styles.edImage} src={education} alt='Students' />
           <div className={styles.edCover}>
@@ -28,6 +50,23 @@ const Services = () => {
         </div>
         <div className={styles.codes}>Affiliate Codes</div>
       </div>
+      {isToggled && (
+        <LayoutGroup>
+          <motion.div
+            layoutId='hair'
+            onClick={handleClose}
+            className={styles.overlay}
+            initial={{ opacity: 0, scale: 0.95 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 0.95 }}
+            transition={{ duration: 1, ease: "easeIn"}}
+          >
+            <div className={styles.overlayContent}>
+              {isToggled === 'hair' && <h1>Hair tips & products</h1>}
+            </div>
+          </motion.div>
+        </LayoutGroup>
+      )}
     </>
   )
 }
