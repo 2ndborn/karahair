2727f76bed2f005f81148a4b0efd8ee8e78c3ea3
2ndborn <micah.francis69@googlemail.com>
2025-08-03 15:43:57 +0100

    Added the content for the education panel and changed to components

---
+++ kara-app/src/components/HairComponent.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { HairData } from '../serviceData/HairData'
+import styles from '../styles/Services.module.css'
+import { AnimateButton } from '../utils/AnimateButton'
+
+const HairComponent = () => {
+    return (
+        <div>
+            <div>
+                <h1>Haircare</h1>
+                <p>
+                    Hair artistry with care at the centre. All services are tailored for
+                    all hair textures, using health-conscious products that protect the
+                    integrity of your hair and scalp. We work with colour ranges derived
+                    from natural ingredients, infused with coconut oil, PPD-free formulas,
+                    and vegan options.
+                </p>
+            </div>
+            <div className={styles.serviceContainer}>
+                {HairData.map((d) => (
+                    <div key={d.index} className={styles.grid}>
+                        <h2>{d.heading}</h2>
+                        <p>{d.content}</p>
+                    </div>
+                ))}
+            </div>
+            <div className={styles.contactButtonWrapper}>
+                <AnimateButton className={styles.contactButton}>
+                    Book Consultation now!!!
+                </AnimateButton>
+            </div>
+            <div>
+                <p>
+                    Please note: A patch test is required at least 48 hours prior to any
+                    colour service your safety and scalp health always come first.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default HairComponent
--- kara-app/src/pages/Services.js
+++ kara-app/src/pages/Services.js
@@ -9,6 +9,9 @@ import Title from '../components/Title';
 import { OverlaySection } from '../utils/OverlaySection';
 import { SectionTile } from '../utils/SectionTile';
 import { HairData } from '../serviceData/HairData';
+import { AnimateButton } from '../utils/AnimateButton';
+import HairComponent from '../components/HairComponent';
+import EducationComponent from '../components/EducationComponent';
 
 const Services = () => {
 
@@ -74,40 +77,8 @@ const Services = () => {
             <div className={styles.closeButton}>
                 <i class="fa-solid fa-xmark"></i>
             </div>
-            {isToggled === 'hair' && (
-              <div>
-                <div>
-                  <h1>Haircare</h1>
-                  <p>
-                    Hair artistry with care at the centre. All services are tailored for
-                    all hair textures, using health-conscious products that protect the
-                    integrity of your hair and scalp. We work with colour ranges derived
-                    from natural ingredients, infused with coconut oil, PPD-free formulas,
-                    and vegan options.
-                  </p>
-                </div>
-                <div className={styles.serviceContainer}>
-                  {HairData.map((d) => (
-                    <div key={d.index} className={styles.grid}>
-                      <h2>{d.heading}</h2>
-                      <p>{d.content}</p>
-                    </div>
-                  ))}
-                </div>
-                  <div className={styles.contactButtonWrapper}>
-                    <button className={styles.contactButton}>
-                      Book Consultation now!!!
-                    </button>
-                  </div>
-                <div>
-                  <p>
-                    Please note: A patch test is required at least 48 hours prior to any
-                    colour service your safety and scalp health always come first.
-                  </p>
-                </div>
-              </div>
-            )}
-            {isToggled === 'educate' && <h1>Education resources & guides</h1>}
+            {isToggled === 'hair' && (<HairComponent />)}
+            {isToggled === 'educate' && <EducationComponent />}
           </OverlaySection>
         )}
         </AnimatePresence>
