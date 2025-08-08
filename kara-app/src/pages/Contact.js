84c8d6d087b88a4a28a548610a07fdce3976a2d3
2ndborn <micah.francis69@googlemail.com>
2025-08-08 19:49:36 +0000

    Styling the contact me page

--- kara-app/src/pages/Contact.js
+++ kara-app/src/pages/Contact.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import styles from '../styles/Contact.module.css';
+import color from '../assets/colorchart.webp';
 
 const Contact = () => {
   return (
     <div className={styles.contactContainer}>
+      <img className={styles.colorChart} src={color} alt='colour chart' />
+      <div className={styles.colorOverlay}></div>
       <div className={styles.formContainer}>
-        <h2>Contact me</h2>
-      <form action="/action_page.php" method="get">
-        <input type='text' name='name' placeholder='Name' />
-        <input type='email' name='email' placeholder='Email Address' />
-        <textarea name='query' placeholder='Your message'></textarea>
-        <button type='submit' value='Submit'>Send</button>
-      </form>
+        <h2>Request a Consultation</h2>
+        <form action="/action_page.php" method="get">
+          <input className={styles.contactInput} type='text' name='name' placeholder='Name' required />
+          <input className={styles.contactInput} type='email' name='email' placeholder='Email Address' required />
+          <textarea className={styles.contactInput} name='query' placeholder='Your message' required></textarea>
+          <button type='submit' value='Submit'>Send</button>
+        </form>
       </div>
     </div>
   )
