c13597564361344fcdc22eb0a09e2da6bd493d9f
2ndborn <micah.francis69@googlemail.com>
2025-08-08 12:59:09 +0000

    Started to create the contact me form

--- kara-app/src/pages/Contact.js
+++ kara-app/src/pages/Contact.js
@@ -1,14 +1,18 @@
 import React from 'react'
+import styles from '../styles/Contact.module.css';
 
 const Contact = () => {
   return (
-    <div>
-        <form action="/action_page.php" method="get">
-            <input type='text' name='name' placeholder='Name' />
-            <input type='email' name='email' placeholder='Email Address' />
-            <textarea name='query' placeholder='Your message'></textarea>
-            <button type='submit' value='Submit'>Send</button>
-        </form>
+    <div className={styles.contactContainer}>
+      <div className={styles.formContainer}>
+        <h2>Contact me</h2>
+      <form action="/action_page.php" method="get">
+        <input type='text' name='name' placeholder='Name' />
+        <input type='email' name='email' placeholder='Email Address' />
+        <textarea name='query' placeholder='Your message'></textarea>
+        <button type='submit' value='Submit'>Send</button>
+      </form>
+      </div>
     </div>
   )
 }
