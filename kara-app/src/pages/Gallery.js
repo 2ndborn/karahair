5e44591cef130ed0eeee90de7675072f9cd37a99
2ndborn <micah.francis69@googlemail.com>
2025-08-14 23:16:18 +0100

    structuring a new layout for the story page

--- kara-app/src/pages/Gallery.js
+++ kara-app/src/pages/Gallery.js
@@ -1,20 +1,39 @@
-import React from 'react'
+import {React, useRef}  from 'react'
 import styles from '../styles/Gallery.module.css'
-import { motion, useTransform } from 'framer-motion';
+import { motion, useTransform, useScroll } from 'framer-motion';
 
 const Gallery = () => {
+  
+  const ref = useRef(null);
+  const boxRef = useRef(null);
+const { scrollYProgress: boxScrollY } = useScroll({
+  target: boxRef,
+  offset: ["start end", "start start"] // triggers when box enters and reaches top
+});
+  const scale = useTransform(boxScrollY, [0, 0.9], [1, 0.65])
   return (
     <div>
-        <section>
-          <div className={styles.intro}>
-            <h1>Hi I am Kara</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam sed repellendus maiores possimus expedita
-              consequuntur nam. Similique, maxime esse! Ullam deserunt numquam id laboriosam in temporibus quod eveniet corrupti
-              ducimus.
-            </p>
-          </div>
-          <div className={styles.con}>
+        <section ref={ref} className={styles.sec}>
+          <motion.div 
+          
+          className={styles.intro}
+          
+          >
+            <motion.div
+            className={styles.text}
+            style={{
+            transformOrigin: "left center",
+            scale }}
+            >
+              <h1>Hi I am Kara</h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam sed repellendus maiores possimus expedita
+                consequuntur nam. Similique, maxime esse! Ullam deserunt numquam id laboriosam in temporibus quod eveniet corrupti
+                ducimus.
+              </p>
+            </motion.div>
+          </motion.div>
+          <div className={styles.con} ref={boxRef}>
             <div className={styles.container}>
               <div className={styles.box}></div>
             </div>
