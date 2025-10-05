46345c6ae68cc37625487e1cd5b4de2280418b75
Micah Francis <micah.francis69@googlemail.com>
2025-10-05 15:47:42 +0100

    installed masonry for thte gallery and uploaded images

--- kara-app/src/pages/Gallery.js
+++ kara-app/src/pages/Gallery.js
@@ -1,9 +1,35 @@
 import { React} from 'react'
+import Masonry, {ResponsiveMasonry} from "react-responsive-masonry"
 
 
 const Gallery = () => {
+
+  const images = [
+    "https://picsum.photos/200/300?image=1050",
+    "https://picsum.photos/300/300?image=206",
+    "https://res.cloudinary.com/ddfubj6vf/image/upload/q_auto,f_auto,w_600,c_fill,g_face/multicolor_oyeevr.jpg",
+    "https://res.cloudinary.com/ddfubj6vf/image/upload/q_auto,f_auto,w_600,c_fill,g_face/alex_a5okil.jpg",
+    "https://res.cloudinary.com/ddfubj6vf/image/upload/q_auto,f_auto,w_600,c_fill,g_face/roc_ateauu.jpg",
+    "https://res.cloudinary.com/ddfubj6vf/image/upload/q_auto,f_auto,w_600,c_fill,g_face/alex_a5okil.jpg",
+  ]
+
   return(
-    <div>Hello world</div>
+    <div>
+      <ResponsiveMasonry
+        columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}
+      >
+        <Masonry>
+          {images.map((image, i) => (
+            <img
+              key={i}
+              src={image}
+              style={{ width: "100%", display: "block" }}
+              alt=""
+            />
+          ))}
+        </Masonry>
+      </ResponsiveMasonry>
+    </div>
   )
 }
 
