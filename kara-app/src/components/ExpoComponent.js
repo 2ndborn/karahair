6c016c1f7be43277546357802439a2ba6f1bfc00
Micah Francis <micah.francis69@googlemail.com>
2025-10-20 22:22:38 +0100

    Finished side scroll in expo component

--- kara-app/src/components/ExpoComponent.js
+++ kara-app/src/components/ExpoComponent.js
@@ -4,20 +4,27 @@ import React, { useRef } from 'react'
 const ExpoComponent = () => {
     const ref = useRef(null);
     const {scrollYProgress} = useScroll({target: ref})
-    const x = useTransform(scrollYProgress, [0,1], ["100%", "-95%"])
+    const x = useTransform(scrollYProgress, [0,1], ["80%", "-95%"])
 
     const data = ["K","N","O","W","L","E","D","G","E"]
 
   return (
-      <div ref={ref} style={{ position: "relative", display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", height: "400vh", backgroundColor: "grey" }}>
-          <div style={{display: "flex"}}>
-              {data.map((d) =>
-                  <motion.div style={{ x, position: "sticky", top: "100px", display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: "black", padding: "1rem", margin: "0 5px", }}>
-                      <h1 style={{ color: "white", fontSize: "20rem" }}>{d}</h1>
-                  </motion.div>
-              )}
+    <>
+      <section ref={ref} style={{position: "relative", height: "250vh"}}>
+          <div  style={{position: "sticky", top: "100px", height: "80vh", display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", overflow: "hidden"}}>
+              <motion.div style={{x, display: "flex" }}>
+                  {data.map((d) =>
+                      <motion.div style={{ display: "flex", justifyContent: "center", alignItems: "center", margin: "0 5px"}}>
+                          <h1 style={{ color: "transparent", fontSize: "20rem", fontWeight: 800, WebkitTextStroke: "10px #fff" }}>{d}</h1>
+                      </motion.div>
+                  )}
+              </motion.div>
           </div>
-    </div>
+      </section>
+      <section style={{height: "200vh"}}>
+          
+      </section>
+    </>
   )
 }
 
