461a7bdfa83755b10af411ddefe04c8f06f16ff2
Micah Francis <micah.francis69@googlemail.com>
2025-10-21 22:20:51 +0100

    Added images to expo component

--- kara-app/src/components/ExpoComponent.js
+++ kara-app/src/components/ExpoComponent.js
@@ -1,16 +1,18 @@
 import { motion, useScroll, useTransform } from 'motion/react';
 import React, { useRef } from 'react'
+import styles from '../styles/HomePage.module.css';
+import colorchart from '../assets/colorchart.webp'
 
 const ExpoComponent = () => {
     const ref = useRef(null);
     const {scrollYProgress} = useScroll({target: ref})
-    const x = useTransform(scrollYProgress, [0,1], ["80%", "-95%"])
+    const x = useTransform(scrollYProgress, [0,1], ["80%", "-80%"])
 
     const data = ["K","N","O","W","L","E","D","G","E"]
 
   return (
     <>
-      <section ref={ref} style={{position: "relative", height: "250vh"}}>
+      <section ref={ref} style={{position: "relative", height: "200vh"}}>
           <div  style={{position: "sticky", top: "100px", height: "80vh", display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", overflow: "hidden"}}>
               <motion.div style={{x, display: "flex" }}>
                   {data.map((d) =>
@@ -21,8 +23,27 @@ const ExpoComponent = () => {
               </motion.div>
           </div>
       </section>
-      <section style={{height: "200vh"}}>
-          
+      <section className={styles.cont}>
+        <div style={{ textAlign: "center", backgroundColor: "#000" }}>
+          <h3 >Knowledge</h3>
+        </div>
+        <div className={styles.expoContainer}>
+          <div className={styles.content}>
+            <p style={{ padding: "min(5rem, 8%)", fontSize: "1.25rem" }}>
+              Your comfort and confidence are my highest priorities.
+              For over two decades, I’ve had the privilege of supporting
+              a diverse range of clients with their hair needs.
+              Through years of dedicated practice, my skills in nurturing,
+              styling, and colouring have evolved to an expert level ensuring
+              every client feels seen, celebrated, and beautifully cared for.
+            </p>
+          </div>
+          <div className={styles.content}>
+            <div style={{ height: "65%", width: "80%", backgroundColor: "#000" }}>
+              <img src={colorchart} alt='color' style={{ height: "100%", width: "100%", backgroundPosition: "cover" }} />
+            </div>
+          </div>
+        </div>
       </section>
     </>
   )
