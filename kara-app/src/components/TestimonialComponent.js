d7468c3c3d36c87de71a954bca004dd70c15faf8
Micah Francis <micah.francis69@googlemail.com>
2025-11-02 20:01:31 +0000

    Finished styling the testimonial overlay

--- kara-app/src/components/TestimonialComponent.js
+++ kara-app/src/components/TestimonialComponent.js
@@ -1,9 +1,46 @@
 import React from 'react'
 
 const TestimonialComponent = () => {
+  const TestimonialData = [
+    {
+      id: 1,
+      content: "Working with Kara was excellent, she is a credit to the industry.",
+      person: "Magi"
+    }, 
+    {
+      id: 2,
+      content: "Working with Kara was excellent, she is a credit to the industry.",
+      person: "Magi"
+    }, 
+  ]
+
   return (
     <div>
+      <div style={{marginBottom: "50px"}}>
         <h1>Testimonials</h1>
+      </div>
+      {TestimonialData.map((test, idx) =>
+        <div key={test.id}>
+          <div style={{ padding: "3rem", fontStyle: "italic", border: "1px solid #535353ff", borderRadius: "10px" }}>
+            <div style={{ textAlign: "left" }}>
+              <p>
+                Working with Kara was excellent, she is a credit to the industry.
+              </p>
+            </div>
+            <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+              <div style={{ color: 'yellow' }}>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+              </div>
+              <h5> - {test.person}</h5>
+            </div>
+          </div>
+          {idx !== TestimonialData.length - 1 && <hr style={{background: "#fff", height: "1px", margin: "2rem auto", border: "none"}}/>}
+        </div>
+      )}
     </div>
   )
 }
