8ab48edac82ca3d80877b79547ed8ec6501b8af8
Micah Francis <micah.francis69@googlemail.com>
2025-11-02 10:55:53 +0000

    Added content to the work ovcomponent

--- kara-app/src/components/WorkComponent.js
+++ kara-app/src/components/WorkComponent.js
@@ -3,7 +3,15 @@ import React from 'react';
 const WorkComponent = () => {
   return (
     <div>
+      <div>
         <h1>Work History</h1>
+      </div>
+      <div>
+        <h2>Toni & Guy</h2>
+        <p>
+          This is where my career begins 
+        </p>
+      </div>
     </div>
   )
 }
--- kara-app/src/serviceData/paragraphData.js
+++ kara-app/src/serviceData/paragraphData.js
@@ -16,8 +16,8 @@ export const paragraphs = [
     },
     {
       id: 2,
-      primary: `Hair is my craft, but education is my purpose.`,
-      secondary: `I run cutting edge courses built for the next
+      primary: `Hair is my craft, but education is my purpose. `,
+      secondary: ` I run cutting edge courses built for the next
       wave of stylists those who want to break rules the right 
       way.`,
       image: teaching,
@@ -25,7 +25,7 @@ export const paragraphs = [
     },{
       id: 3,
       primary: `My work doesn’t just follow trends it helps 
-            create them.`,
+            create them. `,
       secondary: `From the salon floor to the set, I’ve coloured for 
             major productions like: `,
       image: wedding,
@@ -46,13 +46,5 @@ export const paragraphs = [
                 experience, vision, and straight-up passion.`,
       image: brazil,
       button: 'Testimonials'
-    },{
-      id: 5,
-      primary: `Whether you’re sat in my chair or learning in my
-              classroom, you’re getting more than just hair, `,
-      secondary: `you’re getting
-                experience, vision, and straight-up passion.`,
-      image: brazil,
-      button: ''
     },
   ];
