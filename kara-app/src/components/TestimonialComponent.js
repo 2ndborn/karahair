d16745450f8254b60a0989f3d1b557900b0c200d
Micah Francis <micah.francis69@googlemail.com>
2025-11-05 13:30:57 +0000

    Added in some more testimonials

--- kara-app/src/components/TestimonialComponent.js
+++ kara-app/src/components/TestimonialComponent.js
@@ -39,7 +39,28 @@ const TestimonialComponent = () => {
       content: `As always amazing time with Kara... My hair is brought back to 
       life.. thank you so much its beautiful you are one talented lady xxx`,
       person: "Fatima"
-    }, 
+    },
+    {
+      id: 6,
+      content: `I’ve been coming to Kara for over 10 years now, and I honestly wouldn’t 
+      go anywhere else. She’s amazing! Kara has experience with all hair types and has 
+      guided me through so many different styles and colours over the years always knowing 
+      exactly what will suit me best. I absolutely love my visits it’s more than just a 
+      hair appointment, it’s a whole experience. Thank you, Kara, 
+      for always making me feel beautiful!`,
+      person: "Catherine"
+    },
+    {
+      id: 7,
+      content: `I’ve been coming to Kara ever since I moved to London over 15 years 
+      ago, and she’s seen me through all of life’s ups and downs always with a smile. 
+      Kara gives the best advice and truly cares about both hair and wellbeing. 
+      Even when I moved to Paris and tried other stylists, no one could offer the 
+      same level of care, service, or experience. She’s a true artist compassionate, 
+      talented, and genuinely dedicated to her clients. Thank you, Kara, for always 
+      making me feel my best.`,
+      person: "Ginny"
+    },
   ]
 
   return (
@@ -51,9 +72,9 @@ const TestimonialComponent = () => {
         <div key={test.id}>
           <div style={{ padding: "3rem", fontStyle: "italic", border: "1px solid #535353ff", borderRadius: "10px" }}>
             <div style={{ textAlign: "left", fontSize: "1.2rem" }}>
-              <p>{test.content}</p>
+              <p>{test.content}</p><h5> - {test.person}</h5>
             </div>
-            <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+            <div style={{ display: "flex", justifyContent: "flex-end", alignItems: "center" }}>
               <div style={{ color: 'yellow' }}>
                 <i className="fa-solid fa-star"></i>
                 <i className="fa-solid fa-star"></i>
@@ -61,7 +82,6 @@ const TestimonialComponent = () => {
                 <i className="fa-solid fa-star"></i>
                 <i className="fa-solid fa-star"></i>
               </div>
-              <h5> - {test.person}</h5>
             </div>
           </div>
           {idx !== TestimonialData.length - 1 && <hr style={{background: "#fff", height: "1px", margin: "2rem auto", border: "none"}}/>}
