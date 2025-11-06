75781c39295e01afc14dd4f361964d60256876d3
Micah Francis <micah.francis69@googlemail.com>
2025-11-06 08:46:34 +0000

    Amended scroll effect to the intro component

--- kara-app/src/components/IntroScrollComponent.js
+++ kara-app/src/components/IntroScrollComponent.js
@@ -8,7 +8,7 @@ const IntroScrollComponent = () => {
         target: ref,
         offset: ["85% end", "end start"]
     })
-    const scale = useTransform(scrollYProgress, [0, 1], [0, 8]);
+    const scale = useTransform(scrollYProgress, [0.4, 1], [1, 8]);
     const y = useTransform(scrollYProgress, [0, 1], [150, -150])
     const opacityTwo = useTransform(scrollYProgress, [0,0.5, 1], [0, 1, 0])
 
@@ -29,13 +29,26 @@ const IntroScrollComponent = () => {
 
     return (
         <motion.div ref={ref} style={{ opacity, position: "relative", backgroundImage: "linear-gradient(to bottom, #f1e7dd, #ffffff, #fff)", height: "350vh" }}>
-            <motion.div style={{opacity: opacityThree, position: "sticky", top: 0, display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", width: "100%", zIndex: 1}}>
-                <h1>Ready</h1>
+            <motion.div 
+            style={{
+                opacity: opacityThree, 
+                position: "sticky", top: 0, 
+                display: "flex", flexDirection: "column", 
+                justifyContent: "center", alignItems: "center", 
+                height: "100vh", width: "100%", 
+                zIndex: 1, backgroundColor: "lightGray",
+                textAlign: "center", padding: "5rem"
+                }}
+                >
+                <h1 style={{marginBottom: "1.5rem"}}>My Mission</h1>
+                <h3>
+                    The goal is to understand the haircare needs of every individual client, so we can discover the true beauty behind our hair one strand at a time. We can achieve this by following the KARA by model
+                </h3>
             </motion.div>
             <div style={{height: "100vh"}}></div>
             <div style={{ height: "150vh", position: "relative"}}>
-            <motion.div style={{opacity: opacityTwo, position: "sticky", top: 0, display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", width: "100%", zIndex: 2}}>
-                <h1>Steady</h1>
+            <motion.div style={{opacity: opacityTwo, scale, position: "sticky", top: 0, display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", width: "100%", zIndex: 2}}>
+                <h1>Welcome to the KARA Method:</h1>
             </motion.div>
             </div>
             {/* <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
