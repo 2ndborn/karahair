51b028f44577b56ef6d37fcc64066f9eccb956a5
Micah Francis <micah.francis69@googlemail.com>
2025-11-06 13:58:36 +0000

    Fixed the overflow issue on the introScroll component

--- kara-app/src/components/IntroScrollComponent.js
+++ kara-app/src/components/IntroScrollComponent.js
@@ -11,6 +11,8 @@ const IntroScrollComponent = () => {
     const scale = useTransform(scrollYProgress, [0.4, 1], [1, 8]);
     const y = useTransform(scrollYProgress, [0, 1], [150, -150])
     const opacityTwo = useTransform(scrollYProgress, [0,0.5, 1], [0, 1, 0])
+    const fontSize = useTransform(scrollYProgress, [0.4, 1], [32, 128]); // px values
+    const letterSpacing = useTransform(scrollYProgress, [0.4, 1], ["0px", "4px"]);
 
     // const targeRef = useRef(null);
     const { scrollYProgress: targetYProgress } = useScroll({
@@ -36,7 +38,7 @@ const IntroScrollComponent = () => {
                 display: "flex", flexDirection: "column", 
                 justifyContent: "center", alignItems: "center", 
                 height: "100vh", width: "100%", 
-                zIndex: 1, backgroundColor: "lightGray",
+                zIndex: 1,
                 textAlign: "center", padding: "5rem"
                 }}
                 >
@@ -45,11 +47,19 @@ const IntroScrollComponent = () => {
                     The goal is to understand the haircare needs of every individual client, so we can discover the true beauty behind our hair one strand at a time. We can achieve this by following the KARA by model
                 </h3>
             </motion.div>
-            <div style={{height: "100vh"}}></div>
-            <div style={{ height: "150vh", position: "relative"}}>
-            <motion.div style={{opacity: opacityTwo, scale, position: "sticky", top: 0, display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", width: "100%", zIndex: 2}}>
-                <h1>Welcome to the KARA Method:</h1>
-            </motion.div>
+            <div style={{ height: "100vh" }}></div>
+            <div style={{ position: "sticky", top: 0, height: "100vh", overflow: "hidden"}}>
+                <motion.div style={{ opacity: opacityTwo, scale, display: "flex", justifyContent: "center", alignItems: "center", height: "100vh"}}>
+                    <motion.h1
+                        style={{
+                            letterSpacing,
+                            margin: 0,
+                            whiteSpace: "nowrap", // optional: prevents wrapping
+                        }}
+                    >
+                        Welcome to the KARA Method:
+                    </motion.h1>
+                </motion.div>
             </div>
             {/* <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
                 <motion.p style={{
