70cb4ab76111ccc2e62b57066e56e49ad7313fdb
Micah Francis <micah.francis69@googlemail.com>
2025-11-08 15:24:00 +0000

    mades changes to support deployment to github

--- kara-app/src/index.js
+++ kara-app/src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter as Router} from 'react-router-dom';
+// import {BrowserRouter as Router} from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 const isDev = process.env.NODE_ENV === 'development';
 const AppWrapper = isDev ? React.StrictMode : React.Fragment;
